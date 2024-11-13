23fefc3c0b54432f114b8c9060172a0ea06f4be2
mohammedh <mohammedh@zeta.tech>
2024-11-13 19:30:09 +0530

    routing fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,50 @@ import Header from "./Components/Header";
 import "./App.scss";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
 import Footer from "./Components/Footer";
 
+// Define your routes
+const routes = [
+  {
+    path: "/",
+    element: (
+      <>
+        <Header />
+        <Home />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "/products",
+    element: (
+      <>
+        <Header />
+        <Products />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "/products/:productName",
+    element: (
+      <>
+        <Header />
+        <ProductDetails />
+        <Footer />
+      </>
+    ),
+  },
+  { path: "*", element: <NotFound /> },
+];
+
+const router = createBrowserRouter(routes);
+
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:productName" element={<ProductDetails />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
