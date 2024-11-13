5946ccd27dc1b0f4890e946c65b4f2ae9d1a9018
mohammedh <mohammedh@zeta.tech>
2024-11-13 18:36:27 +0530

    minor change

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
 import Header from "./Components/Header";
 import "./App.scss";
-import { lazy, Suspense } from "react";
 import NotFound from "./pages/NotFound";
-const Home = lazy(() => import("./pages/Home"));
+import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Products from "./pages/Products";
-import ProductDetails from './pages/ProductDetails'
+import ProductDetails from "./pages/ProductDetails";
 import Footer from "./Components/Footer";
 
 function App() {
   return (
     <Router>
       <Header />
-        <Routes>
-          <Route path="/" element={
-             <Suspense fallback={<div className="loader">Loading...</div>}>
-             <Home />
-           </Suspense>
-          } />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:productName" element={<ProductDetails />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productName" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
